91b90c9fe0f934ea1de4e72f71aa0f4531d3dcac
LEEDAESEUNG-Developer <eotmd1016@gmail.com>
2022-12-07 15:04:19 +0900

    <깃 저장소에 배포>

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     baseURL : "https://api.themoviedb.org/3",
     params: {
-        api_key: "08cb8ea467ef1b78f3ad40cb782de3ee",
+        api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
         language: "ko-KR",
     },
 });
--- src/components/Row.js
+++ src/components/Row.js
@@ -3,6 +3,17 @@ import axios from "../api/axios";
 import "./Row.css";
 import MovieModal from "./MovieModal";
 
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
 export default function Row({title, fetchUrl, isLargeRow, id}) {
     const [movies, setMovies] = useState([]);
     const [modalOpen, setModalOpen] = useState(false);
@@ -12,7 +23,7 @@ export default function Row({title, fetchUrl, isLargeRow, id}) {
         fetchMovieData();
     }, [fetchUrl]);
 
-    const fetchMovieData = async () =>{
+    const fetchMovieData = async () => {
         const request = await axios.get(fetchUrl);
         console.log("request => ", request);
         setMovies(request.data.results);
@@ -28,34 +39,45 @@ export default function Row({title, fetchUrl, isLargeRow, id}) {
         <section className="row">
             {/*TITLE*/}
             <h2>{title}</h2>
-            <div className="slider">
-                <div className="slider__arrow-left">
-                    <span className="arrow" onClick={() => {
-                        document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-                    }}>
-                        {"<"}
-                    </span>
-                </div>
-                <div id={id} className="row__posters">
-                    {movies.map((movie) => (
+            <Swiper
+                // install Swiper modules
+                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                navigation
+                pagination={{ clickable: true }}
+                loop={true}
+                breakpoints={{
+                    1378:{
+                        slidesPerView: 6,
+                        slidesPerGroup: 6,
+                    },
+                    998:{
+                        slidesPerView: 5,
+                        slidesPerGroup: 5,
+                    },
+                    625:{
+                        slidesPerView: 4,
+                        slidesPerGroup: 4,
+                    },
+                    0:{
+                        slidesPerView: 3,
+                        slidesPerGroup: 3,
+                    }
+                }}
+            >
+            <div id={id} className="row__posters">
+                {movies.map((movie) => (
+                    <SwiperSlide key={movie.id}>
                         <img
-                        key={movie.id}
-                        onClick={() => handleClick(movie)}
-                        className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-                        src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-                        loading="lazy"
-                        alt={movie.name}
+                            onClick={() => handleClick(movie)}
+                            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                            src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                            loading="lazy"
+                            alt={movie.name}
                         />
-                    ))}
-                </div>
-                <div className="slider__arrow-right">
-                    <span className="arrow" onClick={() => {
-                        document.getElementById(id).scrollLeft += window.innerWidth - 80;
-                    }}>
-                        {">"}
-                    </span>
-                </div>
+                    </SwiperSlide>
+                ))}
             </div>
+            </Swiper>
 
             {
                 modalOpen && (
